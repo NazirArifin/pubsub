f78ab981e2201c351ade3f6db9296d98ae584329
Umar. Mansyur <umar.ovie@gmail.com>
2022-08-30 15:21:17 +0700

    haaaiii

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import mqtt from 'mqtt';
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database:'db_sirlon',
+  connectionLimit: 5
+});
 
 const client = mqtt.connect(
   `mqtt://test.mosquitto.org:1883`,
@@ -18,5 +27,12 @@ client.on('connect', () => {
     console.log('subscribed');
   }).on('message', (topic, message) => {
     console.log(topic, message.toString());
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query("INSERT INTO tbl_air VALUES (NULL, 8977, NOW())");
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
